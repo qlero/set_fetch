2c586922957137a7e349332312b4a4619a551e47
LMquentinLR <quentin.leroux@edhec.com>
2021-10-21 16:01:13 +0200

    BLAS install for Python/Manjaro

--- _Manjaro_setup/4_setup_python.sh
+++ _Manjaro_setup/4_setup_python.sh
@@ -1,5 +1,6 @@
 #!/bin/bash/
 
+sudo pacman -S fltk # install the BLAS library for fast optimization
 sudo pacman -Syyu 
 sudo pacman -Syu python-pip
 pip install numpy pandas matplotlib seaborn numba tensorflow
