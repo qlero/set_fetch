7ad2d49bc2885f6039657f776f201dcc2c576456
LMquentinLR <quentin.leroux@edhec.com>
2021-10-21 16:20:38 +0200

    Manjaro setup: Python, R

--- _Manjaro_setup/4_setup_python.sh
+++ _Manjaro_setup/4_setup_python.sh
@@ -3,4 +3,6 @@
 sudo pacman -S fltk # install the BLAS library for fast optimization
 sudo pacman -Syyu 
 sudo pacman -Syu python-pip
-pip install numpy pandas matplotlib seaborn numba tensorflow
+pip install scipy numpy pandas matplotlib seaborn numba tensorflow keras dask pytorch gudhi
+sudo pacman -Syu jupyter-notebook
+export JUPYTERLAB_DIR=$HOME/.local/share/jupyter/lab
--- _Manjaro_setup/6_setup_rstudio.sh
+++ _Manjaro_setup/6_setup_rstudio.sh
@@ -11,3 +11,19 @@ cd yaourt
 makepkg -si
 sudo pacman -Sy yaourt
 yaourt -S rstudio-desktop-bin
+
+# R Kernel in Jupyter 
+# https://simply-python.com/2019/06/24/running-r-on-jupyter-notebook-with-r-kernel-no-anaconda/
+#Objectives:
+#Install R Kernel on Jupyter Notebook (Windows)
+#Required Tools:
+#R for windows— R for windows
+#JupyterNotebook — Jupyter Notebook
+#Steps:
+#Install R. Use the R terminal (do not use R studio) to install R packages:
+#  install.packages(c(‘repr’, ‘IRdisplay’, ‘evaluate’, ‘crayon’, ‘pbdZMQ’, ‘devtools’, ‘uuid’, ‘digest’))
+#  install.packages(‘IRkernel’)
+#Make Kernel available to Jupyter
+#  IRkernel::installspec()
+#  OR IRkernel::installspec(user = FALSE) #install system-wide
+#Open a notebook and open new R script.
