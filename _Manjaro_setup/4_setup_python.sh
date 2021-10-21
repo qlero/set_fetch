5072a0d36b3e8894d4f895420d9643018335064d
LMquentinLR <quentin.leroux@edhec.com>
2021-10-21 22:38:03 +0200

    update _Manjaro setup, python

--- _Manjaro_setup/4_setup_python.sh
+++ _Manjaro_setup/4_setup_python.sh
@@ -1,8 +1,21 @@
 #!/bin/bash/
 
-sudo pacman -S fltk # install the BLAS library for fast optimization
+#sudo pacman -S fltk # install the BLAS library for fast optimization
 sudo pacman -Syyu 
-sudo pacman -Syu python-pip
-pip install scipy numpy pandas matplotlib seaborn numba tensorflow keras dask pytorch gudhi
-sudo pacman -Syu jupyter-notebook
-export JUPYTERLAB_DIR=$HOME/.local/share/jupyter/lab
+#sudo pacman -Syu python-pip
+#pip install scipy numpy pandas matplotlib seaborn numba tensorflow keras dask pytorch gudhi
+#sudo pacman -Syu jupyter-notebook
+#export JUPYTERLAB_DIR=$HOME/.local/share/jupyter/lab
+
+# LINK
+
+cd ~/Downloads
+LATEST_ANACONDA=$(wget -O - https://www.anaconda.com/distribution/ 2>/dev/null | sed -ne 's@.*\(https:\/\/repo\.anaconda\.com\/archive\/Anaconda3-.*-Linux-x86_64\.sh\)\">64-Bit (x86) Installer.*@\1@p')
+wget $LATEST_ANACONDA
+chmod +x Anaconda3*.sh # make it executable
+./Anaconda3*.sh # execute the installer
+conda install -c conda-forge notebook
+conda install -c conda-forge nb_conda_kernels
+conda install -c conda-forge jupyter_contrib_nbextensions
+conda install pip
+conda create -n ML_base tensorflow-gpu pip ipykernel gudhi matplotlib seaborn scipy numpy dask pytorch
