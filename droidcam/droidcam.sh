29c961f8b7dcc17ef2d150ee1f0b81d6907b882e
LMquentinLR <quentin.leroux@edhec.com>
2021-10-27 14:45:16 +0200

    repository cleanup

--- droidcam/droidcam.sh
+++ droidcam/droidcam.sh
@@ -1,6 +1,11 @@
-# Install and launch droidcam
+#!/bin/bash
+
+#
+# Script to install and launch droidcam: to use an android phone as a webcam
+#
 
 cd /tmp/
+# check if the link is still the most up to date
 wget -O droidcam_latest.zip https://files.dev47apps.net/linux/droidcam_1.7.1.zip
 unzip droidcam_latest.zip -d droidcam
 cd droidcam && sudo ./install-client
