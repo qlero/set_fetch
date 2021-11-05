f9c8761b6f80cdb798cd4f094f6690cad1411683
LMquentinLR <quentin.leroux@edhec.com>
2021-11-05 12:11:38 +0100

    droidcam update

--- droidcam/droidcam.sh
+++ droidcam/droidcam.sh
@@ -4,11 +4,21 @@
 # Script to install and launch droidcam: to use an android phone as a webcam
 #
 
+sudo pacman -Syu dkms base-devel --needed
+
+# Reboot afterwards.
+
+sudo pacman -S gcc make
+sudo pacman -S $(ls /boot | awk -F "-" '/^linux/ { print $1"-headers" }')
+
 cd /tmp/
 # check if the link is still the most up to date
-wget -O droidcam_latest.zip https://files.dev47apps.net/linux/droidcam_1.7.1.zip
+wget -O droidcam_latest.zip https://files.dev47apps.net/linux/droidcam_1.8.0.zip
+# sha1sum: 3f94d6ec57a191ad146392fcf094b205e375e1ef
 unzip droidcam_latest.zip -d droidcam
 cd droidcam && sudo ./install-client
-sudo apt install linux-headers-`uname -r` gcc make
+
+#for debian/ubuntu
+#sudo apt install linux-headers-`uname -r` gcc make
+#sudo pacman -S linux-headers gcc make
 sudo ./install-video
-droidcam
