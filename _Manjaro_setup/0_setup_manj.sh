9f0e8fe3a99b61ed6c81a1c18a9347a4d610ced8
qlero <quentin.leroux@edhec.com>
2021-11-26 22:48:09 +0100

    vimrc update + manjaro setup

--- _Manjaro_setup/0_setup_manj.sh
+++ _Manjaro_setup/0_setup_manj.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+sudo rm /var/lib/pacman/db.lck
 sudo pacman -Syyu rlwrap cmake ffmpeg
 sudo pacman -Syyu openblas libx11
+pamac install base-devel 
 mkdir ~/Downloads/tmp
 git clone https://aur.archlinux.org/yay-git.git ~/Downloads/tmp
 cd ~/Downloads/tmp/yay
